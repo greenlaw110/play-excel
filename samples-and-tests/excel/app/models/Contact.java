fe72da9ad5749fb1fa3732f9547a455fc84beb8e
greenlaw110 <greenlaw110@gmail.com>
2011-06-15 11:23:32 +1000

    proj re-organize

---
+++ samples-and-tests/excel/app/models/Contact.java
@@ -0,0 +1,37 @@
+package models;
+
+import javax.persistence.Entity;
+
+import play.db.jpa.Model;
+import play.jobs.Job;
+import play.jobs.OnApplicationStart;
+import play.test.Fixtures;
+
+@Entity
+public class Contact extends Model {
+
+    private static final long serialVersionUID = -5806820381004668188L;
+
+    public String firstName;
+    public String lastName;
+    public String title;
+    public String address;
+    public String mobile;
+    public String email;
+
+    public String toString() {
+        // Contact c = Contact.find("").first();
+        // c = Contact.findById(c);
+        return title + " " + firstName + " " + lastName;
+    }
+
+    @OnApplicationStart
+    public static class BootLoader extends Job<Object> {
+        @Override
+        public void doJob() {
+            if (Contact.count() == 0) {
+                Fixtures.load("initial-data.yml");
+            }
+        }
+    }
+}
--- src/play/modules/excel/Excel.java
+++ src/play/modules/excel/Excel.java
@@ -25,7 +25,6 @@ package play.modules.excel;
 
 import java.util.List;
 
-import play.Logger;
 import play.Play;
 import play.classloading.enhancers.LocalvariablesNamesEnhancer;
 import play.data.validation.Validation;
@@ -37,82 +36,95 @@ import play.mvc.Scope;
 
 public class Excel {
 
-	/**
-	 * Render a specific template
-	 * 
-	 * @param templateName
-	 *            The template name
-	 * @param args
-	 *            The template data
-	 */
-	public static void renderTemplateAsExcel(String templateName, Object... args) {
+    private String getTemplateName_(String templateName) {
+        if (templateName.matches(""))
+            return templateName;
+        return "TODO";
+    }
 
-		// Template datas
-		Scope.RenderArgs templateBinding = Scope.RenderArgs.current();
-		for (Object o : args) {
-			List<String> names = LocalvariablesNamesEnhancer.LocalVariablesNamesTracer
-					.getAllLocalVariableNames(o);
-			for (String name : names) {
-				templateBinding.put(name, o);
-			}
-		}
-		templateBinding.put("session", Scope.Session.current());
-		templateBinding.put("request", Http.Request.current());
-		templateBinding.put("flash", Scope.Flash.current());
-		templateBinding.put("params", Scope.Params.current());
-		if (null == templateBinding.get("fileName")) {
-			templateBinding.put("fileName", templateName.substring(templateName.lastIndexOf("/") + 1) + ".xls");
-		}
-		//Logger.trace("fileName: " + templateBinding.get("fileName"));
-		try {
-			templateBinding.put("errors", Validation.errors());
-		} catch (Exception ex) {
-			throw new UnexpectedException(ex);
-		}
-		try {
-			throw new RenderExcelTemplate(templateName + ".xls", templateBinding.data, templateBinding.get("fileName").toString());
-		} catch (TemplateNotFoundException ex) {
-			if (ex.isSourceAvailable()) {
-				throw ex;
-			}
-			StackTraceElement element = PlayException
-					.getInterestingStrackTraceElement(ex);
-			if (element != null) {
-				throw new TemplateNotFoundException(templateName, Play.classes
-						.getApplicationClass(element.getClassName()), element
-						.getLineNumber());
-			} else {
-				throw ex;
-			}
-		}
-	}
+    /**
+     * Render a specific template
+     * 
+     * @param templateName
+     *            The template name
+     * @param args
+     *            The template data
+     */
+    public static void renderTemplateAsExcel(String templateName,
+            Object... args) {
 
-	/**
-	 * Render the corresponding template
-	 * 
-	 * @param args
-	 *            The template data
-	 */
-	public static void renderExcel(Object... args) {
-		String templateName = null;
-		final Http.Request request = Http.Request.current();
+        // Template datas
+        Scope.RenderArgs templateBinding = Scope.RenderArgs.current();
+        for (Object o : args) {
+            List<String> names = LocalvariablesNamesEnhancer.LocalVariablesNamesTracer
+                    .getAllLocalVariableNames(o);
+            for (String name : names) {
+                templateBinding.put(name, o);
+            }
+        }
+        templateBinding.put("session", Scope.Session.current());
+        templateBinding.put("request", Http.Request.current());
+        templateBinding.put("flash", Scope.Flash.current());
+        templateBinding.put("params", Scope.Params.current());
+        Object fileName = templateBinding.get("fileName");
+        if (null == fileName) {
+            templateBinding.put("fileName",
+                    templateName.substring(templateName.lastIndexOf("/") + 1)
+                            + ".xls");
+        }
+        // Logger.trace("fileName: " + templateBinding.get("fileName"));
+        try {
+            templateBinding.put("errors", Validation.errors());
+        } catch (Exception ex) {
+            throw new UnexpectedException(ex);
+        }
+        try {
+            throw new RenderExcelTemplate(templateName + ".xls",
+                    templateBinding.data, templateBinding.get("fileName")
+                            .toString());
+        } catch (TemplateNotFoundException ex) {
+            if (ex.isSourceAvailable()) {
+                throw ex;
+            }
+            StackTraceElement element = PlayException
+                    .getInterestingStrackTraceElement(ex);
+            if (element != null) {
+                throw new TemplateNotFoundException(
+                        templateName,
+                        Play.classes.getApplicationClass(element.getClassName()),
+                        element.getLineNumber());
+            } else {
+                throw ex;
+            }
+        }
+    }
 
-		if (args.length > 0
-				&& args[0] instanceof String
-				&& LocalvariablesNamesEnhancer.LocalVariablesNamesTracer
-						.getAllLocalVariableNames(args[0]).isEmpty()) {
-			templateName = args[0].toString();
-		} else {
-			templateName = request.action.replace(".", "/");
-		}
-		if (templateName.startsWith("@")) {
-			templateName = templateName.substring(1);
-			if (!templateName.contains(".")) {
-				templateName = request.controller + "." + templateName;
-			}
-			templateName = templateName.replace(".", "/");
-		}
-		renderTemplateAsExcel(templateName, args);
-	}
+    /**
+     * Render the corresponding template
+     * 
+     * @param args
+     *            The template data
+     */
+    public static void renderExcel(Object... args) {
+        String templateName = null;
+        final Http.Request request = Http.Request.current();
+
+        if (args.length > 0
+                && args[0] instanceof String
+                && LocalvariablesNamesEnhancer.LocalVariablesNamesTracer
+                        .getAllLocalVariableNames(args[0]).isEmpty()) {
+            templateName = args[0].toString();
+        } else {
+            templateName = request.action.replace(".", "/");
+        }
+        if (templateName.startsWith("@")) {
+            templateName = templateName.substring(1);
+            if (!templateName.contains(".")) {
+                templateName = request.controller + "." + templateName;
+            }
+            templateName = templateName.replace(".", "/");
+        }
+        renderTemplateAsExcel(templateName, args);
+    }
 
 }
